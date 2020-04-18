5ff9ca84c35266c839a124f76f8a3fe2753faf7c
Luke <lukehenry04@gmail.com>
2020-04-18 02:39:26 -0400

    Day1 - 2am Progress
    Simple Prototype with rotation and movement done

---
+++ LD46 - Keep It Alive/Assets/CharController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharController : MonoBehaviour
+{
+    Rigidbody rb;
+    public Transform mesh;
+    public Rigidbody planet;
+    public float mouseSensitivity = 300f;
+    public float grav = -10;
+    public float speed;
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        rb.useGravity = false;
+       
+    }
+
+   
+    void FixedUpdate()
+    {
+        //GRAVITY & ROTATION
+        //Point from planet to rb
+        Vector3 target = rb.position - planet.position;
+        target.Normalize();
+        //Rotate to face planet
+        rb.MoveRotation(Quaternion.FromToRotation(transform.up, target) * rb.rotation);
+        //apply gravity
+        rb.AddForce(target * grav);
+
+        //ROTATION
+        //rotate in movement dir
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            mesh.rotation =
+                Quaternion.FromToRotation(
+                    mesh.forward,
+                    ((Input.GetAxis("Horizontal") * transform.right) + (Input.GetAxis("Vertical") * transform.forward)).normalized
+                ) * mesh.rotation
+           ;
+            mesh.SetPositionAndRotation(mesh.position, Quaternion.FromToRotation(mesh.up,transform.up)*mesh.rotation);
+        }
+
+        //MOVEMENT
+        //Move position perp to rotation, or tangent to planet, ie, move in transform.right positions instead of Vector3.right... Local vs Global / Subjective v Objective...
+        rb.MovePosition(rb.position + (Input.GetAxis("Horizontal") * speed * transform.right) + (Input.GetAxis("Vertical") * speed * transform.forward));
+
+     
+    }
+}
